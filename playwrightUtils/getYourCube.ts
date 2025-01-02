d23d718bb8abd918a0bdc1e301b90aa5687e0783
James Riley <jamesriley72281@gmail..com>
2025-01-02 15:43:48 -0600

    added comments

--- playwrightUtils/getYourCube.ts
+++ playwrightUtils/getYourCube.ts
@@ -1,5 +1,6 @@
 import { Page } from 'playwright'; // Import Page from Playwright
 
 export async function getYourCube(page: Page, name: string, cards: string, followers: string) {
+  // TODO: add QA selector to the element below so we can use a brittle selector.
   return page.getByRole('link', { name: `${name} ${cards} Card Cube ${followers}` });
 }
