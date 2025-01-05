6985ba9f3d5570fa6afb9a38dae919eb28d5c38c
James Riley <jamesriley72281@gmail..com>
2025-01-05 12:26:24 -0600

    added scripts to run UI or headed easily

---
+++ playwrightData/cardData 2.ts
@@ -0,0 +1,10 @@
+export const testCardSoldierOfFortune = {
+  name: 'Soldier of Fortune',
+  version: 'ALL-80',
+  manaValue: '1',
+};
+export const testCardMoxDiamond = {
+  name: 'Mox Diamond',
+  version: '[sth-138]',
+  manaValue: '0',
+};
