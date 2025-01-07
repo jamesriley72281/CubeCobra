bdb0df2365034513cc2f3895f1cba0940e0ed72b
James Riley <jamesriley72281@gmail..com>
2025-01-07 11:03:13 -0600

    removed files

--- playwrightUtils/getYourCube 2.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from 'playwright';
-
-export async function getYourCube(page: Page, name: string, cards: string, followers: string) {
-  // TODO: add QA selector to the element below so we can use a less brittle selector.
-  return page.getByRole('link', { name: `${name} ${cards} Card Cube ${followers}` });
-}
